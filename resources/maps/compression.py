e940c1bbaa341e262bd77604302e4ef20e50655a
Simon Meersschaut <simon.meersschaut@gmail.com>
2025-05-07 22:03:10 +0200

    Add some compressed zones

--- resources/maps/compression.py
+++ resources/maps/compression.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 from PIL import Image
 
 
-def compress(zone_id)
+def compress(zone_id):
     with Image.open(f"zones/{zone_id}/map.png") as img:
         # img.getbbox
         img.width
@@ -12,38 +12,20 @@ def compress(zone_id)
         GRID = (img.width, img.height)  # columns, rows
 
 
+    # Adjust the ratio for the cropped image
     RATIO = (GRID[0] / IMAGE_SIZE[0], GRID[1] / IMAGE_SIZE[1])
 
-    # Crop parameters
-    CROP_TOPLEFT = (230, 230)
-    CROP_BOTTOMRIGHT = (845, 1370)
-
-    # Adjusted image size after cropping
-    CROPPED_IMAGE_SIZE = (
-        CROP_BOTTOMRIGHT[0] - CROP_TOPLEFT[0],
-        CROP_BOTTOMRIGHT[1] - CROP_TOPLEFT[1],
-    )
-
-    # Adjust the ratio for the cropped image
-    RATIO = (GRID[0] / CROPPED_IMAGE_SIZE[0], GRID[1] / CROPPED_IMAGE_SIZE[1])
-
-
-
     # Load rectangle data
-    with open(f"resources/maps/zones/{zone_id}/rectangles.json", "r") as f:
+    with open(f"zones/{zone_id}/rectangles.json", "r") as f:
         data = json.load(f)
 
     output_data = {}
     for seat in data:
-        # Adjust coordinates for the crop
-        adjusted_x = seat['coordinates']['x'] - CROP_TOPLEFT[0]
-        adjusted_y = seat['coordinates']['y'] - CROP_TOPLEFT[1]
-
         # Calculate grid positions
-        column_start = round(adjusted_x * RATIO[0])
-        column_end = round((adjusted_x + seat['coordinates']['width'] + 2) * RATIO[0])
-        row_start = round(adjusted_y * RATIO[1])
-        row_end = round((adjusted_y + seat['coordinates']['height'] + 4) * RATIO[1])
+        column_start = round(seat['coordinates']['x'] * RATIO[0])
+        column_end = round((seat['coordinates']['x'] + seat['coordinates']['width'] + 2) * RATIO[0])
+        row_start = round(seat['coordinates']['y'] * RATIO[1])
+        row_end = round((seat['coordinates']['y'] + seat['coordinates']['height'] + 4) * RATIO[1])
 
         output_data.update({
             seat['id']: f"{column_start}/{column_end};{row_start}/{row_end}"
@@ -52,8 +34,17 @@ def compress(zone_id)
     # Sort the output data by identifier
     sorted_output_data = OrderedDict(sorted(output_data.items(), key=lambda x: int(x[0])))
 
+    # add config data
+    sorted_output_data.update({
+        "config": {
+            "columns": GRID[0],
+            "rows": GRID[1],
+        }
+    })
+    sorted_output_data.move_to_end("config", last=False) # insert at beginning (for a nice layout)
+
     # Save the updated rectangle data
-    with open(f"resources/maps/zones/{zone_id}/compression.json", "w+") as f:
+    with open(f"zones/{zone_id}/compression.json", "w+") as f:
         json.dump(sorted_output_data, f, indent=4)
 
 
@@ -63,7 +54,11 @@ def compress(zone_id)
 
 
 def main():
-    compress(1)
-
+    for i in range(1, 62):
+        try:
+            compress(i)
+        except:
+            print("error")
+    
 if __name__ == "__main__":
     main()
