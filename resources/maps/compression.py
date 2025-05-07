16735dad57d54bd226dcdf5c05e8cf5c92181850
Simon Meersschaut <simon.meersschaut@gmail.com>
2025-05-07 21:55:08 +0200

    Upload rectangles 1 - 62

--- resources/maps/compression.py
+++ resources/maps/compression.py
@@ -2,62 +2,68 @@ import json
 from collections import OrderedDict
 from PIL import Image
 
-ZONE_ID = 2
 
-IMAGE_SIZE = (1060, 1500)  # width, height
-GRID = (424, 600)  # columns, rows
-
-RATIO = (GRID[0] / IMAGE_SIZE[0], GRID[1] / IMAGE_SIZE[1])
-
-# Crop parameters
-CROP_TOPLEFT = (230, 230)
-CROP_BOTTOMRIGHT = (845, 1370)
-
-# Adjusted image size after cropping
-CROPPED_IMAGE_SIZE = (
-    CROP_BOTTOMRIGHT[0] - CROP_TOPLEFT[0],
-    CROP_BOTTOMRIGHT[1] - CROP_TOPLEFT[1],
-)
-
-# Adjust the ratio for the cropped image
-RATIO = (GRID[0] / CROPPED_IMAGE_SIZE[0], GRID[1] / CROPPED_IMAGE_SIZE[1])
-
-# Crop the image
-original_image_path = f"resources/maps/zones/{ZONE_ID}/original.png"
-cropped_image_path = f"resources/maps/zones/{ZONE_ID}/map.png"
-
-with Image.open(original_image_path) as img:
-    cropped_img = img.crop((*CROP_TOPLEFT, *CROP_BOTTOMRIGHT))
-    cropped_img.save(cropped_image_path)
-
-# Load rectangle data
-with open(f"resources/maps/zones/{ZONE_ID}/rectangles.json", "r") as f:
-    data = json.load(f)
-
-output_data = {}
-for seat in data:
-    # Adjust coordinates for the crop
-    adjusted_x = seat['coordinates']['x'] - CROP_TOPLEFT[0]
-    adjusted_y = seat['coordinates']['y'] - CROP_TOPLEFT[1]
-
-    # Calculate grid positions
-    column_start = round(adjusted_x * RATIO[0])
-    column_end = round((adjusted_x + seat['coordinates']['width'] + 2) * RATIO[0])
-    row_start = round(adjusted_y * RATIO[1])
-    row_end = round((adjusted_y + seat['coordinates']['height'] + 4) * RATIO[1])
-
-    output_data.update({
-        seat['id']: f"{column_start}/{column_end};{row_start}/{row_end}"
-    })
-
-# Sort the output data by identifier
-sorted_output_data = OrderedDict(sorted(output_data.items(), key=lambda x: int(x[0])))
-
-# Save the updated rectangle data
-with open(f"resources/maps/zones/{ZONE_ID}/compression.json", "w+") as f:
-    json.dump(sorted_output_data, f, indent=4)
+def compress(zone_id)
+    with Image.open(f"zones/{zone_id}/map.png") as img:
+        # img.getbbox
+        img.width
+        img.height
+        IMAGE_SIZE = (img.width, img.height)  # width, height
+        GRID = (img.width, img.height)  # columns, rows
 
 
-new_grid_width = round(424 * (615 / 1060))  # ≈ 246
-new_grid_height = round(600 * (1140 / 1500))  # ≈ 456
-print(f"WARNING: Please change the aspect ratio (in the css) to: '{new_grid_width} / {new_grid_height}'")
+    RATIO = (GRID[0] / IMAGE_SIZE[0], GRID[1] / IMAGE_SIZE[1])
+
+    # Crop parameters
+    CROP_TOPLEFT = (230, 230)
+    CROP_BOTTOMRIGHT = (845, 1370)
+
+    # Adjusted image size after cropping
+    CROPPED_IMAGE_SIZE = (
+        CROP_BOTTOMRIGHT[0] - CROP_TOPLEFT[0],
+        CROP_BOTTOMRIGHT[1] - CROP_TOPLEFT[1],
+    )
+
+    # Adjust the ratio for the cropped image
+    RATIO = (GRID[0] / CROPPED_IMAGE_SIZE[0], GRID[1] / CROPPED_IMAGE_SIZE[1])
+
+
+
+    # Load rectangle data
+    with open(f"resources/maps/zones/{zone_id}/rectangles.json", "r") as f:
+        data = json.load(f)
+
+    output_data = {}
+    for seat in data:
+        # Adjust coordinates for the crop
+        adjusted_x = seat['coordinates']['x'] - CROP_TOPLEFT[0]
+        adjusted_y = seat['coordinates']['y'] - CROP_TOPLEFT[1]
+
+        # Calculate grid positions
+        column_start = round(adjusted_x * RATIO[0])
+        column_end = round((adjusted_x + seat['coordinates']['width'] + 2) * RATIO[0])
+        row_start = round(adjusted_y * RATIO[1])
+        row_end = round((adjusted_y + seat['coordinates']['height'] + 4) * RATIO[1])
+
+        output_data.update({
+            seat['id']: f"{column_start}/{column_end};{row_start}/{row_end}"
+        })
+
+    # Sort the output data by identifier
+    sorted_output_data = OrderedDict(sorted(output_data.items(), key=lambda x: int(x[0])))
+
+    # Save the updated rectangle data
+    with open(f"resources/maps/zones/{zone_id}/compression.json", "w+") as f:
+        json.dump(sorted_output_data, f, indent=4)
+
+
+    new_grid_width = round(424 * (615 / 1060))  # ≈ 246
+    new_grid_height = round(600 * (1140 / 1500))  # ≈ 456
+    print(f"WARNING: Please change the aspect ratio (in the css) to: '{new_grid_width} / {new_grid_height}'")
+
+
+def main():
+    compress(1)
+
+if __name__ == "__main__":
+    main()
--- resources/maps/fetch_data.py
+++ resources/maps/fetch_data.py
@@ -1,19 +1,32 @@
 import requests
-
-
+import os
+import json
+import shutil
 
 def load_zone(zone_id: int, cookies):
     output = []
     # Load general information
     resp = requests.get(f"https://kurt3.ghum.kuleuven.be/api/zones/{zone_id}", cookies=cookies)
-    print(resp.status_code)
-    print(resp.text)
-    location_id = resp.json()["locationId"]
-    
+    data = resp.json()
+    if "status" in data.keys() and data["status"] != 200:
+        with open("errors.txt", "a") as f:
+            f.write(f"{zone_id}\n")
+        if "title" in data.keys():
+            # some error occured (probably an overlflow on the zone_id)
+            print(data["title"])
+        return
+    location_id = data["locationId"]
+    floor_plan_id = data["floorPlanId"]
+
     # Load data
     resp = requests.get(f"https://kurt3.ghum.kuleuven.be/api/zoneavailabilities?locationId={location_id}&zoneId={zone_id}&startDate=2025-05-07&startTime=21:00", cookies=cookies)
-    
-    for seat in resp.json()["availabilities"]:
+    data = resp.json()
+    if not "availabilities" in data.keys():
+        print(resp.json())
+        with open("errors.txt", "a") as f:
+            f.write(f"{zone_id}\n")
+        return
+    for seat in data["availabilities"]:
         output.append({
             "id": seat['resourceName'].split(' ')[-1],
             "coordinates": {
@@ -23,6 +36,22 @@ def load_zone(zone_id: int, cookies):
                 "height": 25
             }
         })
+    # create directory
+    if not os.path.exists(f"zones/{zone_id}"):
+        os.mkdir(f"zones/{zone_id}")
+
+    # Write rectangles file
+    with open(f"zones/{zone_id}/rectangles.json", 'w+') as f:
+        json.dump(output, f)
+
+    # Fetch image
+    url = f"https://kurtfloorplan.blob.core.windows.net/floorplans/FloorPlan_{floor_plan_id}.png"
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(f"zones/{zone_id}/map.png", 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)    
+
     print(output)
 
 def main():
@@ -32,7 +61,9 @@ def main():
         for entry in f.read().split("; "):
             cookies.update({entry.split("=")[0]: entry.split("=")[1]})
     # load
-    load_zone(2, cookies)
+    for i in range(1, 70): # zone 0 does not exist
+        # input(i)
+        load_zone(i, cookies)
 
 if __name__ == "__main__":
     main()
