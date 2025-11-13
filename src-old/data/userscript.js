27f4d87e8ca837a58019f2038b8e70046417cf28
Simon Meersschaut <simon.meersschaut@gmail.com>
2025-11-13 18:19:47 +0100

    Remove old source code

--- src-old/data/userscript.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         KURT-PRO
-// @description  KURT-PRO is a webbrowser extension that aims to provide students a better experience when using the KULeuven KURT reservation system.
-// @author       S. Meersschaut
-// @version      $VERSION$
-// @match        *://kurt3.ghum.kuleuven.be/*
-// @grant        none
-// @inject-into  content
-// ==/UserScript==
--- src-old/scripts/banner.js
+++
@@ -1,9 +0,0 @@
-/*
-Generates the DOM content, which will be placed inside the banner (when you are on the original site).
-*/
-function createBanner(){
-    const banner = document.createElement("button");
-    banner.innerText = "KURT-PRO";
-    banner.onclick = () => {window.location.assign("?kurt-pro");};
-    return banner;
-}
